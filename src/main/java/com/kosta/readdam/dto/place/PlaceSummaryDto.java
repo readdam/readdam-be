be5ae391c309f694f1279611b6021a4331f90983
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-06-23 17:55:09 +0900

    Update PlaceSummaryDto.java

--- src/main/java/com/kosta/readdam/dto/place/PlaceSummaryDto.java
+++ src/main/java/com/kosta/readdam/dto/place/PlaceSummaryDto.java
@@ -16,6 +16,7 @@ public class PlaceSummaryDto {
     private Long roomCount;
     private List<String> tags;      // ← 태그 최대 10개
     private String thumbnailImage;  // ← 대표 이미지 (가장 먼저 등록된 사진)
+    private List<String> images;
     private List<String> weekdayTime;
     private List<String> weekendTime;
     private Integer likeCount;
