e97508c54f441aad58472c22e7d06c08e702703a
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-07-03 11:12:55 +0900

    refactor: 책 리뷰 작성 시 리뷰 개수, 평점 업데이트

---
+++ src/main/java/com/kosta/readdam/repository/reservation/ReservationTimeResponse.java
@@ -0,0 +1,17 @@
+package com.kosta.readdam.repository.reservation;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationTimeResponse {
+
+    private List<String> allTimes;
+    private List<String> reservedTimes;
+    private List<String> availableTimes;
+}
